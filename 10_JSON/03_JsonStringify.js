4cf425e0de3346404a730f0e4a0d2edc9fbfe460
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2024-01-02 15:17:41 -0300

    Aula 03) JSON Stringify

---
+++ 10_JSON/03_JsonStringify.js
@@ -0,0 +1,16 @@
+const OBJ = { name: "John", age: 30, city: "New York" };
+const JSON_STRING = JSON.stringify(OBJ);
+console.log(JSON_STRING);
+
+/*
+     Funciona somente quando os itens do objeto são, TODOS,
+     pertencentes a um tipo que se tem também em arquivos JSON:
+      -> Números
+      -> String
+      -> Booleans
+      -> Arrays
+      -> Objetos
+
+     Portanto, funções e tipos pertencentes a uma classe, como Datas
+     não funcionam em stringify
+*/
