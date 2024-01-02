42e598788175bfba6573203922b3c8d8bfea042d
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2024-01-02 15:18:13 -0300

    modified:   10_JSON/03_JsonStringify.js

--- 10_JSON/03_JsonStringify.js
+++ 10_JSON/03_JsonStringify.js
@@ -5,11 +5,13 @@ console.log(JSON_STRING);
 /*
      Funciona somente quando os itens do objeto são, TODOS,
      pertencentes a um tipo que se tem também em arquivos JSON:
+     
       -> Números
       -> String
       -> Booleans
       -> Arrays
       -> Objetos
+      -> null
 
      Portanto, funções e tipos pertencentes a uma classe, como Datas
      não funcionam em stringify
